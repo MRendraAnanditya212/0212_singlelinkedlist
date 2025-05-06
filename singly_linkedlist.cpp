6da7fbb5bb025523c27cdfc76f63bc2665e9cb0e
MRendraAnanditya212 <mrendraananditya@gmail.com>
2025-05-06 13:56:46 +0700

    membuat case 5

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -155,17 +155,27 @@ int main()
         mhs.traverse();
       }
       break;
-      
-        case '5':
-    {
-      exit(0);
-    }
-    break;
-    default:
-    {
-      cout << "Pilihan Salah !." << endl;
-    }
-    break;
+      case '4':
+      {
+        if (mhs.listEmpty() == true)
+        {
+          cout << "\nList Kosong\n";
+          break;
+        }
+        Node *previous, *current;
+        cout << endl << "Masukkan no Mahasiswa yang dicari : ";
+        cin >> nim;
+        if (mhs.Search(nim, &previous ,&current) == false)
+          cout << endl << "Data tidak ditemukan" << endl;
+        else
+        {
+            cout << endl << "Data ditemukan" << endl;
+            cout << "\nNo Mahasiswa : " << current->noMhs << endl;
+            cout << "\n";
+          }
+        }
+        break;
+        
   }
  }
 }
