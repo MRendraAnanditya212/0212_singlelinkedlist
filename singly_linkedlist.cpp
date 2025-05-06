f6f9c58fe509d1623395ecd1d06e3a2587f997e5
MRendraAnanditya212 <mrendraananditya@gmail.com>
2025-05-06 13:54:00 +0700

    membuat case 2

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -115,22 +115,27 @@ int main()
  linkedList mhs;
  int nim;
  char ch;
- 
-    case '2':
+ while(1)
+ {
+  cout << endl
+       << "Menu";
+  cout << endl
+       << "1. Menambah data kedalam list" << endl;
+  cout << "2. Menghapus data dari dalam list" << endl;
+  cout << "3. Menampilkan semua data didalam list" << endl;
+  cout << "4. Mencari data dalam list" << endl;
+  cout << "5. keluar" << endl;
+  cout << endl
+       << "Masukkan pilihan (1-5) : ";
+  cin >> ch;
+  switch (ch)
+  {
+    case '1':
     {
-      if (mhs.listEmpty())
-      {
-        cout << endl << "list Kosong" << endl;
-        break;
-      }
-      cout << endl << "\nMasukkna no Mahasiswa yang akan dihapus : ";
-      cin >> nim;
-      if (mhs.delNode(nim) == false)
-        cout << endl << "Data tidak ditemukan" << endl;
-      else
-        cout << endl << "Data dengan nomor Mahasiswa" << nim << "berhasil dihapus " << endl;
+      mhs.addNode();
     }
     break;
+    
     case '3': 
     {
         mhs.traverse();
